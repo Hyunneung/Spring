5116eefdb4cbb42cab8f904ae470e5581b960f3c
Hyunneung <neong_@naver.com>
2022-09-01 21:39:13 +0900

    annotation - @@Autowired

---
+++ example13/HelloApp.java
@@ -0,0 +1,14 @@
+package org.zerock.controller.example13;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		GenericXmlApplicationContext gxac = new GenericXmlApplicationContext("org/zerock/controller/example13/applicationContext.xml");
+		MessageBean bean = (MessageBean)gxac.getBean("m");
+		bean.sayHello();
+		gxac.close();
+	}
+
+}
---
+++ example13/MessageBean.java
@@ -0,0 +1,5 @@
+package org.zerock.controller.example13;
+
+public interface MessageBean {
+	void sayHello();
+}
