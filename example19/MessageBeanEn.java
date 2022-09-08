18fa630a94173036ed5192f6b91864a186525cb9
Hyunneung <104151569+Hyunneung@users.noreply.github.com>
2022-09-08 23:41:08 +0900

    initMethod() 메소드 - @Component / @Resource / @PostConstruct

--- example19/MessageBeanEn.java
+++ example19/MessageBeanEn.java
@@ -9,14 +9,14 @@ public class MessageBeanEn implements MessageBean {
 	@Override
 	public void sayHello(String name) {
 		System.out.println("Hello!" + name);
-	}
+	} 
 	
 	public MessageBeanEn() {
-		System.out.println("MessageBeanEn »ı¼ºÀÚ");
+		System.out.println("MessageBeanEn ìƒì„±ì");
 	}
 	
 	@PreDestroy
 	public void destroyMethod() {
-		System.out.println("MessageBeanEnÀÇ destroyMethod() ¸Ş¼Òµå");
+		System.out.println("MessageBeanEnÃ€Ã‡ destroyMethod() ë©”ì†Œë“œ");
 	}
 }
